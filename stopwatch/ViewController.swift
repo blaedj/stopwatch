4e45c8ddbad9ee8d57e3dae74148f0d8dd0a4b0f
blaedj <blaedj@gmail.com>
2015-06-03 06:31:33 -0500

    basic functionality working

--- stopwatch/ViewController.swift
+++ stopwatch/ViewController.swift
@@ -10,16 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+
+    var timer = NSTimer()
+    var counter = 0
+    
+    func updateTime() {
+        counter++
+        timeLabel.text = "\(counter)"
+    }
+    
+    @IBAction func pause(sender: AnyObject) {
+        timer.invalidate()
+    }
+ 
+    @IBAction func play(sender: AnyObject) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("updateTime"), userInfo:nil, repeats: true)
+    }
+
+    
+    @IBAction func stop(sender: AnyObject) {
+        timer.invalidate()
+        counter = 0
+        timeLabel.text = "0.0"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
