27cd98c787b3e217a601bdf1f17db0126674abd8
blaedj <blaedj@gmail.com>
2015-06-10 15:55:38 -0500

    one button for play and pause, increment by 1/10 of a second

--- stopwatch/ViewController.swift
+++ stopwatch/ViewController.swift
@@ -12,31 +12,55 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var timeLabel: UILabel!
 
+    @IBOutlet weak var toolbar: UIToolbar!
+   
     var timer = NSTimer()
-    var counter = 0
+    var counter = 0.0
+    var playPauseButton = UIBarButtonItem()
+    var clearButton = UIBarButtonItem()
+    var flexSpacer = UIBarButtonItem()
     
     func updateTime() {
-        counter++
+        counter = round((counter + 0.1) * 10) / 10
+        print(counter)
+        print("\n")
         timeLabel.text = "\(counter)"
     }
     
-    @IBAction func pause(sender: AnyObject) {
-        timer.invalidate()
-    }
- 
-    @IBAction func play(sender: AnyObject) {
-        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("updateTime"), userInfo:nil, repeats: true)
+    func toggleOnOff() {
+
+        if(!timer.valid) {
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("updateTime"), userInfo:nil, repeats: true)
+            togglePlayPauseButton(UIBarButtonSystemItem.Pause)
+        } else {
+            timer.invalidate()
+            togglePlayPauseButton(UIBarButtonSystemItem.Play)
+        }
     }
 
     
-    @IBAction func stop(sender: AnyObject) {
-        timer.invalidate()
+    func togglePlayPauseButton(style: UIBarButtonSystemItem){
+        playPauseButton = UIBarButtonItem(barButtonSystemItem: style, target: self, action: Selector("toggleOnOff"))
+        var buttons = [playPauseButton, flexSpacer, clearButton]
+        toolbar.setItems(buttons, animated: false)
+    }
+    
+    func stop() {
+        if timer.valid {
+            timer.invalidate()
+            togglePlayPauseButton(UIBarButtonSystemItem.Play)
+        }
         counter = 0
         timeLabel.text = "0.0"
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        playPauseButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Play, target: self, action: Selector("toggleOnOff"))
+        clearButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Stop, target: self, action: Selector("stop"))
+        flexSpacer = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace, target: self, action: nil)
+        var buttons = [playPauseButton, flexSpacer, clearButton]
+        toolbar.setItems(buttons, animated: false)
     }
 
     override func didReceiveMemoryWarning() {
